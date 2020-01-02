6f13ecba9def4e0bca8a9cd54b518b2ed822dfc3
Vlad-2005-lab <59056097+Vlad-2005-lab@users.noreply.github.com>
2020-01-02 15:11:15 +0500

    Add files via upload

--- a.py
+++ a.py
@@ -1,57 +1,355 @@
 import pygame
 import os
 import sys
-from body import *
+import math
+import random
+from time import sleep
+from taimer import *
 
 
-class Timer:
-    def __init__(self):
-        self.dtime = 0
-        self.htime = 0
-        self.mtime = 0
-        self.stime = 0
+class Boss:
+    HP = 9999999999
+    armor = 9999999999
+    damage = 9999999999
 
-    def tick(self, time=1):
-        global heal_pc, pc
-        for i in range(time):
-            pc.HP += pc.regen
-            self.stime += 1
-            if self.stime == 60:
-                self.stime = 0
-                self.mtime += 1
-            if self.mtime == 60:
-                self.mtime = 0
-                self.htime += 1
-            if self.htime == 24:
-                self.htime = 0
-                self.dtime += 1
-            if self.dtime == 99:
-                terminate()
-            if pc.HP > pc.SHP:
-                pc.HP = pc.SHP
-        heal_pc = 0
-        pc.all_characters()
+    def __init__(self, hard_of_level):
+        if hard_of_level == 1:
+            self.HP = 150000
+            self.armor = 0
+            self.damage = 1250
+        elif hard_of_level == 2:
+            self.HP = 500000
+            self.armor = 500
+            self.damage = 2500
+        elif hard_of_level == 3:
+            self.HP = 1000000
+            self.armor = 2000
+            self.damage = 7500
 
-    def print(self):
-        d = str(self.dtime) if len(str(self.dtime)) == 2 else '0' + str(self.dtime)
-        h = str(self.htime) if len(str(self.htime)) == 2 else '0' + str(self.htime)
-        m = str(self.mtime) if len(str(self.mtime)) == 2 else '0' + str(self.mtime)
-        s = str(self.stime) if len(str(self.stime)) == 2 else '0' + str(self.stime)
-        return d + ':' + h + ':' + m + ':' + s
+    def taking_damage(self, hard_of_level):
+        if hard_of_level != 3:
+            if pc.damage - (
+                    self.armor - pc.physical_penetration) > 0 and \
+                    self.armor - pc.physical_penetration > 0:
+                self.HP -= (pc.damage - (self.armor - pc.physical_penetration))
+        else:
+            if random.choice([True, False]):
+                if pc.damage - (
+                        self.armor - pc.physical_penetration) > 0 and \
+                        self.armor - pc.physical_penetration > 0:
+                    self.HP -= (pc.damage - (self.armor - pc.physical_penetration))
+
+    def giving_damage(self):
+        pc.taking_damage()
 
 
+class miniBoss:
+    HP = 9999999999
+    armor = 9999999999
+    damage = 9999999999
+
+    def __init__(self, hard_of_level):
+        if hard_of_level == 1:
+            self.HP = 15000
+            self.armor = 0
+            self.damage = 125
+        elif hard_of_level == 2:
+            self.HP = 50000
+            self.armor = 50
+            self.damage = 250
+        elif hard_of_level == 3:
+            self.HP = 100000
+            self.armor = 200
+            self.damage = 750
+
+    def taking_damage(self, hard_of_level):
+        if hard_of_level != 3:
+            if pc.damage - (self.armor - pc.physical_penetration) > 0 and \
+                    self.armor - pc.physical_penetration > 0:
+                self.HP -= (pc.damage - (self.armor - pc.physical_penetration))
+        else:
+            if random.choice([True, False]):
+                if pc.damage - (self.armor - pc.physical_penetration) > 0 and \
+                        self.armor - pc.physical_penetration > 0:
+                    self.HP -= (pc.damage - (self.armor - pc.physical_penetration))
+
+    def giving_damage(self):
+        pc.taking_damage()
+
+
+class Player_characters:
+    SHP = 0
+    armor = 0
+    damage = 0
+    vampirizm = 0
+    regen = 0
+    extra_life = 0
+    HP = SHP
+    dod = 0
+    physical_penetration = 0
+
+    def __init__(self, hard_of_level):
+        if hard_of_level == 1:
+            self.SHP = 100
+            self.HP = self.SHP
+            self.armor = 200
+            self.damage = 100
+            self.regen = 25
+        elif hard_of_level == 2:
+            self.SHP = 50
+            self.HP = self.SHP
+            self.armor = 100
+            self.damage = 20
+            self.regen = 4
+        elif hard_of_level == 3:
+            self.SHP = 10
+            self.HP = self.SHP
+            self.armor = 50
+            self.damage = 0
+            self.regen = 1
+
+    def taking_damage(self):
+        if Boss.damage - self.armor > 0:
+            self.HP -= (Boss.damage - self.armor)
+
+    def giving_damage(self, hero):
+        hero.taking_damage()
+        if self.damage - hero.armor > 0:
+            self.HP += ((self.damage - hero.armor) * (self.vampirizm / 1000))
+            self.HP = math.ceil(self.HP)
+        if hero.HP <= 50 and self.dod > 0:
+            self.dod = 0
+            self.damage = math.ceil(self.damage * 1.25)
+
+    def kill(self):
+        if self.extra_life > 0 and self.HP < 0:
+            self.extra_life -= 1
+            self.SHP = self.SHP * 0.15
+            self.HP = self.SHP
+        else:
+            fon = pygame.transform.scale(load_image('game_over.jpg', True), (1000, 1000))
+            screen.blit(fon, (0, 0))
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        terminate()
+                    elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                        terminate()
+
+
+def blade_of_despair():
+    pc.dod += 1
+    pc.damage += 170
+
+
+def blade_of_the_seven_seas():
+    pc.damage += 65
+    pc.SHP += 250
+    pc.physical_penetration += 15
+
+
+def berserker_rage():
+    pc.damage += 65
+    pc.damage = math.ceil(pc.damage * 1.25)
+
+
+def axe_of_bloodlust():
+    pc.damage += 70
+    pc.vampirizm += 20
+
+
+def endless_battle():
+    pc.damage += 65
+    pc.SHP += 250
+    pc.vampirizm += 15
+
+
+def claws_of_chaos():
+    pc.damage += 70
+    pc.vampirizm += 20
+
+
+def nature_wind():
+    pc.damage += 10
+    pc.vampirizm += 15
+
+
+def armor_blade():
+    pc.armor += 90
+
+
+def benefit_of_courage():
+    pc.SHP += 770
+    pc.vampirizm += 45
+    pc.armor = math.ceil(pc.armor * 1.1)
+    pc.damage = math.ceil(pc.damage * 1.1)
+
+
+def caller_of_the_devil():
+    pc.damage += 15
+    pc.SHP += 770
+
+
+def forse_of_ice():
+    pc.damage += 30
+    pc.SHP += 1000
+
+
+def trident():
+    pc.damage += 80
+
+
+def golden_meteor():
+    pc.damage += 60
+    pc.vampirizm += 5
+
+
+def a_shot_of_the_hunter():
+    pc.damage += 80
+
+
+def the_Golden_stick():
+    pc.damage += 100
+
+
+def the_giants_axe():
+    pc.damage += 30
+    pc.SHP += 250
+
+
+def the_sword_of_the_legionnaire():
+    pc.damage += 60
+
+
+def dagger():
+    pc.damage += 15
+
+
+def an_ordinary_spear():
+    pc.damage += 40
+
+
+def hammer_of_wrath():
+    pc.damage += 35
+    pc.physical_penetration += 15
+
+
+def an_angry_growl():
+    pc.damage += 60
+    pc.physical_penetration += 40
+
+
+def health_crystal():
+    pc.SHP += 230
+
+
+def leather_armor():
+    pc.armor += 15
+
+
+def healing_necklace():
+    pc.regen += 20
+
+
+def the_belt_of_ares():
+    pc.SHP += 770
+
+
+def studded_armor():
+    pc.armor += 90
+
+
+def queens_wings():
+    pc.SHP += 1000
+    pc.damage += 15
+
+
+def storm_belt():
+    pc.SHP += 800
+    pc.armor += 40
+
+
+def protective_helmet():
+    pc.SHP += 1550
+    pc.regen += 100
+
+
+def immortality():
+    pc.armor += 40
+    pc.SHP += 800
+    pc.extra_life += 1
+
+
+def what_the_item(item):
+    if item == 'a':
+        axe_of_bloodlust()
+    elif item == 'b':
+        berserker_rage()
+    elif item == 'c':
+        blade_of_despair()
+    elif item == 'd':
+        blade_of_the_seven_seas()
+    elif item == 'e':
+        claws_of_chaos()
+    elif item == 'g':
+        endless_battle()
+    elif item == 'f':
+        nature_wind()
+    elif item == 'h':
+        a_shot_of_the_hunter()
+    elif item == 'i':
+        an_ordinary_spear()
+    elif item == 'j':
+        armor_blade()
+    elif item == 'k':
+        benefit_of_courage()
+    elif item == 'l':
+        caller_of_the_devil()
+    elif item == 'm':
+        dagger()
+    elif item == 'n':
+        golden_meteor()
+    elif item == 'o':
+        hammer_of_wrath()
+    elif item == 'p':
+        healing_necklace()
+    elif item == 'q':
+        health_crystal()
+    elif item == 'r':
+        leather_armor()
+    elif item == 's':
+        queens_wings()
+    elif item == 'u':
+        storm_belt()
+    elif item == 'v':
+        studded_armor()
+    elif item == 'w':
+        the_Golden_stick()
+    elif item == 'x':
+        the_giants_axe()
+    elif item == 'y':
+        the_sword_of_the_legionnaire()
+    elif item == 't':
+        trident()
+    elif item == 'z':
+        protective_helmet()
+    elif item == 'A':
+        immortality()
+
+
+pygame.init()
 size = width, height = 1000, 1000
 screen = pygame.display.set_mode(size)
 running = True
-t = Timer()
-a = input()
-pc.all_characters()
+# a = input()
+a = 'map.txt'
 if not os.path.exists('data/' + a):
     sys.exit()
 sp = []
 clock = pygame.time.Clock()
 game_map = []
 dikt = {}
+hard_of_level = 0
 sp_of_gotten_things = []
 with open('data/' + a, 'r') as mapFile:
     le = [line.strip() for line in mapFile]
@@ -59,27 +357,67 @@ with open('data/' + a, 'r') as mapFile:
     for el in range(len(le)):
         game_map.append(list(le[el]))
         for ell in range(len(le[el])):
-            if le[el][ell] == '#':
+            if le[el][ell] == '#' or le[el][ell] == 'B':
                 sp.append([ell, el])
 
 coords = [299, 299]
 
 
 def start_screen():
-    fon = pygame.transform.scale(load_image('fon.jpg', True), (1000, 1000))
+    global hard_of_level
+    stop = True
+    fon = pygame.transform.scale(load_image('first_fon.jpg', True), (1000, 1000))
     screen.blit(fon, (0, 0))
-    while True:
+    while stop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                stop = False
+                break
+        pygame.display.flip()
+        clock.tick(FPS)
+    fon = pygame.transform.scale(load_image('second_fon.jpg', True), (1000, 1000))
+    screen.blit(fon, (0, 0))
+    stop = True
+    while stop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN and (event.pos[0] > 208) and (event.pos[1] > 391) and (
+                    event.pos[0] < 784) and (event.pos[1] < 553):
+                hard_of_level = 1
+                stop = False
                 return
+            elif event.type == pygame.MOUSEBUTTONDOWN and (event.pos[0] > 208) and (event.pos[1] > 570) and (
+                    event.pos[0] < 776) and (event.pos[1] < 728):
+                hard_of_level = 2
+                stop = False
+                return
+            elif event.type == pygame.MOUSEBUTTONDOWN and (event.pos[0] > 208) and (event.pos[1] > 745) and (
+                    event.pos[0] < 774) and (event.pos[1] < 902):
+                hard_of_level = 3
+                stop = False
+                return
+
         pygame.display.flip()
         clock.tick(FPS)
 
 
+class Bosss(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(boss_group, all_sprites)
+        self.image = tile_images['boss']
+        self.x = pos_x
+        self.y = pos_y
+        self.rect = self.image.get_rect().move(tile_width * self.x, tile_height * self.y)
+
+    def draw(self):
+        self.rect = self.image.get_rect().move(tile_width * self.x, tile_height * self.y)
+
+
 def load_image(name, color_key=None):
-    fullname = os.path.join('data/', name)
+    fullname = os.path.join('data', name)
     image = pygame.image.load(fullname).convert()
     if color_key is not None:
         if color_key == -1:
@@ -92,7 +430,11 @@ def load_image(name, color_key=None):
         return pygame.transform.scale(image, (50, 50))
 
 
+bx, by = 0, 0
+
+
 def generate_level(level):
+    global bx, by
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
@@ -160,12 +502,14 @@ def generate_level(level):
                 Tile('protective_helmet', x, y)
             elif level[y][x] == 'A':
                 Tile('immortality', x, y)
+            elif level[y][x] == 'B':
+                Tile('boss', x, y)
             dikt[(y, x)] = level[y][x]
     return new_player, x, y
 
 
-tile_images = {'wall': load_image('box.png'), 'empty': load_image('floor.png'),
-               'axe_of_bloodlust': load_image('axe_of_bloodlust.png'),
+tile_images = {'boss': pygame.transform.scale(load_image('boss.png', True), (100, 100)), 'wall': load_image('box.png'),
+               'empty': load_image('floor.png'), 'axe_of_bloodlust': load_image('axe_of_bloodlust.png'),
                'berserker_rage': load_image('berserker_rage.png'),
                'blade_of_despair': load_image('blade_of_despair.png'),
                'blade_of_seven_seas': load_image('blade_of_the_seven_seas.png'),
@@ -187,6 +531,7 @@ tile_images = {'wall': load_image('box.png'), 'empty': load_image('floor.png'),
 player_image = load_image('mar.png', -1)
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
+boss_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 tile_width = tile_height = 50
 
@@ -194,10 +539,26 @@ FPS = 50
 
 
 class Tile(pygame.sprite.Sprite):
+    x = -5
+    y = -5
+    f = False
+
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        if tile_type == 'boss':
+            Tile.x = pos_x
+            Tile.y = pos_y
+            Tile.f = True
+            self.image = tile_images[tile_type]
+            self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        else:
+            if Tile.f and (pos_x == Tile.x + 1 and pos_y == Tile.y) or (
+                    pos_x == Tile.x + 1 and pos_y == Tile.y + 1) or (pos_x == Tile.x and pos_y == Tile.y + 1):
+                self.image = tile_images[tile_type]
+                self.rect = self.image.get_rect().move(-1000, -1000)
+            else:
+                self.image = tile_images[tile_type]
+                self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -246,9 +607,6 @@ class Player(pygame.sprite.Sprite):
             game_map[coords[1]][coords[0]] = '@'
 
 
-#   def to_coords(self):
-#      self.rect.topleft = [coords[0] * ]
-
 def load_level(filename):
     filename = "data/" + filename
     with open(filename, 'r') as mapFile:
@@ -285,10 +643,9 @@ class Camera:
 
 
 heal_pc = 0
+pc = Player_characters(hard_of_level)
 camera = Camera()
-# РёР·РјРµРЅСЏРµРј СЂР°РєСѓСЂСЃ РєР°РјРµСЂС‹
 camera.update(player)
-# РѕР±РЅРѕРІР»СЏРµРј РїРѕР»РѕР¶РµРЅРёРµ РІСЃРµС… СЃРїСЂР°Р№С‚РѕРІ
 for sprite in all_sprites:
     camera.apply(sprite)
 while True:
@@ -309,7 +666,6 @@ while True:
                     player, level_x, level_y = generate_level(game_map)
                     tile_width = tile_height = 50
                     camera.update(player)
-                    pc.all_characters()
                 else:
                     Player.movel(player)
                     camera.update(player)
@@ -328,7 +684,6 @@ while True:
                     player, level_x, level_y = generate_level(game_map)
                     tile_width = tile_height = 50
                     camera.update(player)
-                    pc.all_characters()
                 else:
                     Player.mover(player)
                     camera.update(player)
@@ -347,7 +702,6 @@ while True:
                     player, level_x, level_y = generate_level(game_map)
                     tile_width = tile_height = 50
                     camera.update(player)
-                    pc.all_characters()
                 else:
                     Player.moveu(player)
                     camera.update(player)
@@ -366,21 +720,47 @@ while True:
                     player, level_x, level_y = generate_level(game_map)
                     tile_width = tile_height = 50
                     camera.update(player)
-                    pc.all_characters()
                 else:
                     Player.moved(player)
                     camera.update(player)
                 for sprite in all_sprites:
                     camera.apply(sprite)
-        elif k[pygame.K_t]:
-            t.tick(10)
     heal_pc += 1
     if heal_pc == 25:
-        print(t.print())
         t.tick()
+        if pc.regen >= pc.SHP - pc.HP:
+            pc.HP = pc.SHP
+        else:
+            pc.HP += pc.regen
+        heal_pc = 0
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
     tiles_group.draw(screen)
     player_group.draw(screen)
+    pygame.draw.rect(screen, [255, 0, 0], [700, 0, 1000, 300])
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render('Time: ' + t.print(), 0, (0, 0, 0))
+    screen.blit(text1, (710, 10))
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render(str(pc.HP) + '/' + str(pc.SHP) + 'HP', 0, (0, 0, 0))
+    screen.blit(text1, (710, 48))
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render('armor: ' + str(pc.armor), 0, (0, 0, 0))
+    screen.blit(text1, (710, 86))
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render('damage: ' + str(pc.damage), 0, (0, 0, 0))
+    screen.blit(text1, (710, 124))
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render('vampirizm: ' + str(pc.vampirizm), 0, (0, 0, 0))
+    screen.blit(text1, (710, 160))
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render('regen: ' + str(pc.regen), 0, (0, 0, 0))
+    screen.blit(text1, (710, 198))
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render('extra life: ' + str(pc.extra_life), 0, (0, 0, 0))
+    screen.blit(text1, (710, 236))
+    f1 = pygame.font.Font(None, 28)
+    text1 = f1.render('physical penetration: ' + str(pc.physical_penetration), 0, (0, 0, 0))
+    screen.blit(text1, (710, 274))
     pygame.display.flip()
     clock.tick(FPS)
